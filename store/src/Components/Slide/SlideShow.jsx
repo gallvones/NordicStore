4035683e8c7f9a1201d8a33c8d01169e0696d203
Luiz Galvão <luizmedeiros327@gmail.com>
2025-01-16 19:49:58 -0300

    Doing the first section

--- store/src/Components/Slide/SlideShow.jsx
+++ store/src/Components/Slide/SlideShow.jsx
@@ -1,49 +1,84 @@
 import React from 'react';
 import './SlideShow.css';
 //Slide
-import { register } from 'swiper/element/bundle';
+
 import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
-
-register();
+import 'swiper/css/navigation';
+import { EffectCoverflow, Pagination, Navigation} from 'swiper/modules';
+import slide_image_1 from "../img/ceci.png"
+import slide_image_2 from "../img/b1f.png"
+import slide_image_3 from "../img/b2f.png"
+import slide_image_4 from "../img/b3f.png"
+import slide_image_5 from "../img/b4f.png"
+import slide_image_6 from "../img/photo1.jpg"
+import slide_image_7 from "../img/ceci.png"
 
 
 const SlideShow = () => {
 
-const imgs = [
-  { id: '1', Image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhZ9X_f8yBuGptdg61oQ_EMggsWs9w36fmLVO5nd8aoyvzSgSI6hZ61tZIkSzyiTohJiVkx_4si9ZElYewUR2Xm5ONYjF0dzi4ysfEat5b3DP-LyRD7R8BrhDFKhxzwWDLO4gidyuZa_dQ/s1600/god-of-war.jpg'},
-  { id: '2', Image: 'https://psxbrasil.com.br/wp-content/uploads/2020/09/GOW-3-4K.jpg'},
-  { id: '3', Image: 'https://psxbrasil.com.br/wp-content/uploads/2020/09/GOW-5-1080-2.jpg'},
-]
 
   return (
-    <div className='slide'>
-      <br/>
-      <br/>
-      
-        <Swiper
-        slidesPerView={1}
-        pagination={{ clickable: true}}
-        navigation
-        >
-          <br/>
-          {imgs.map( (item) => (
-            <SwiperSlide key= {item.id}>
-              <img 
-                 src= {item.Image}
-                 alt= "Slider"
-                 className='slide__item'
-                 
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+    <div className='container'>
+      <h1 className="heading"> Camisetas</h1>
+      <br></br><br></br>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" className='slide_image'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" className='slide_image'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" className='slide_image'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" className='slide_image'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" className='slide_image'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" className='slide_image'/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" className='slide_image'/>
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+        </div>
+      </Swiper>
     </div>
-  )
+  );
 }
+
 
 export default SlideShow
