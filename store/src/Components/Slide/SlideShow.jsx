b7a73fab779dcafe5fe3a9ddf7bd7c97d08b1348
Luiz Galvão <luizmedeiros327@gmail.com>
2025-01-17 18:14:07 -0300

    add a new branch fourd. I back to the old slides

--- store/src/Components/Slide/SlideShow.jsx
+++ store/src/Components/Slide/SlideShow.jsx
@@ -1,84 +1,54 @@
 import React from 'react';
 import './SlideShow.css';
+import Load from '../img/loading.jpeg';
 //Slide
-
+import { register } from 'swiper/element/bundle';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import { EffectCoverflow, Pagination, Navigation} from 'swiper/modules';
-import slide_image_1 from "../img/ceci.png"
-import slide_image_2 from "../img/b1f.png"
-import slide_image_3 from "../img/b2f.png"
-import slide_image_4 from "../img/b3f.png"
-import slide_image_5 from "../img/b4f.png"
-import slide_image_6 from "../img/photo1.jpg"
-import slide_image_7 from "../img/ceci.png"
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+register();
 
 
 const SlideShow = () => {
 
+const imgs = [
+  { id: '1', Image: Load},
+  { id: '2', Image: Load},
+  { id: '3', Image: Load},
+]
 
   return (
-    <div className='container'>
-      <h1 className="heading"> Camisetas</h1>
-      <br></br><br></br>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
+    <div className='slide'>
+      <br/>
+      <br/>
+      
+        <Swiper
+        slidesPerView={1}
+        pagination={{ clickable: true}}
+        navigation
+        autoplay={{
+          delay: 4000, 
+          disableOnInteraction: false,
         }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        <SwiperSlide>
-          <img src={slide_image_1} alt="slide_image" className='slide_image'/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_2} alt="slide_image" className='slide_image'/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_3} alt="slide_image" className='slide_image'/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_4} alt="slide_image" className='slide_image'/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" className='slide_image'/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_6} alt="slide_image" className='slide_image'/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_7} alt="slide_image" className='slide_image'/>
-        </SwiperSlide>
-
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </div>
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
-        </div>
-      </Swiper>
+        >
+          <br/><br/>
+          {imgs.map( (item) => (
+            <SwiperSlide key= {item.id}>
+              <img 
+                 src= {item.Image}
+                 alt= "Slider"
+                 className='slide__item'
+                 
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
     </div>
-  );
+  )
 }
-
 
 export default SlideShow
