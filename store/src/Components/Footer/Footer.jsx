c0f86056822fa65f6e78d986bb56034d5b8a6d9d
Luiz Galvão <luizmedeiros327@gmail.com>
2025-02-19 00:56:12 -0300

    Changing the list of footer

--- store/src/Components/Footer/Footer.jsx
+++ store/src/Components/Footer/Footer.jsx
@@ -14,23 +14,8 @@ const Footer = () => {
   return (
     <div className='all_footer'>
     <div className='container_footer'>
-      <div className='list1_footer'>
-        <div> <h2>Sobre</h2></div>
-       <Link to="aboutUs" className='link_aboutUs'>
-        <div><p>NORDIC STORE</p></div>
-        </Link>
-        <div> <p>Since 2025</p></div>
-        
-      </div>
-
-      <div className='list2_footer'>
-     <div><h2>Políticas </h2></div>
-     <div><p>Política de Frete</p></div>
-     <div><p>Política de Cookie</p></div>
-     <div><p>Política de Trocas</p></div>
-      </div>
-
-      <div className='list3_footer'>
+   
+    <div className='list1_footer'>
 <div><h2>Fale com a gente!</h2></div>
 <div><Phone/><p className='list3Phone_footer'>(61) 99983-1708</p></div>
 <div><Mail/> <p>contato@nordicstore.com.br</p></div>
@@ -39,13 +24,30 @@ const Footer = () => {
 <div><p>Seg. a sex. das 11hrs às 17hrs</p></div>
       </div>
     
-    <div className='list4_footer'>
+      <div className='list2_footer'>
 <div><h2>Siga a Nordic!</h2></div>
-  <div className='instaIcon_list4_footer'><Instagram/><p>Instagram</p></div>
-  <div className='zapIcon_list4_footer'><Whatsapp/><p>Whatsapp</p></div>
- <div className='youtubeIcon_list4_footer'> <Youtube/><p>Youtube</p></div>
-  <div className='tiktokIcon_list4_footer'><TikTok/><p>TikTok</p></div>
+  <div className='instaIcon_list2_footer'><Instagram/><p>Instagram</p></div>
+  <div className='zapIcon_list2_footer'><Whatsapp/><p>Whatsapp</p></div>
+ <div className='youtubeIcon_list2_footer'> <Youtube/><p>Youtube</p></div>
+  <div className='tiktokIcon_list2_footer'><TikTok/><p>TikTok</p></div>
     </div>
+
+    <div className='list3_footer'>
+     <div><h2>Políticas </h2></div>
+     <div><p>Política de Frete</p></div>
+     <div><p>Política de Cookie</p></div>
+     <div><p>Política de Trocas</p></div>
+      </div>
+      
+      <div className='list4_footer'>
+        <div> <h2>Sobre</h2></div>
+       <Link to="aboutUs" className='link_aboutUs'>
+        <div><p>NORDIC STORE</p></div>
+        </Link>
+        <div> <p>Since 2025</p></div>
+        
+      </div>
+    
     </div>
 <div className='container_copyright'>
   <h2>Copyright </h2>
