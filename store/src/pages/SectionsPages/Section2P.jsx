47d47d85b3be6d73b57827183402338bf7084fd8
Luiz Galvão <luizmedeiros327@gmail.com>
2025-02-20 17:36:53 -0300

    Sections pages ready to receive the other srcBar and the other Products

--- store/src/pages/SectionsPages/Section2P.jsx
+++ store/src/pages/SectionsPages/Section2P.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import '../SectionsPagesStyles/Section2P.css'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
 const Section2P = () => {
   return (
-    <div>Section2P</div>
+    <div className='all_Section2P'>
+     <Header/>
+<div className='body_Section2P'>
+<div className='filterContent_Section2P'>
+{/* <SrcBar2/> */}
+</div>
+<hr />
+<Footer/>
+</div>
+    </div>
   )
 }
 
--- store/src/pages/SectionsPages/Section3P.jsx
+++ store/src/pages/SectionsPages/Section3P.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import '../SectionsPagesStyles/Section3P.css'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
 const Section3P = () => {
   return (
-    <div>Section3P</div>
+    <div className='all_Section3P'>
+    <Header/>
+<div className='body_Section3P'>
+<div className='filterContent_Section3P'>
+{/* <SrcBar3/> */}
+</div>
+<hr />
+<Footer/>
+</div>
+   </div>
   )
 }
 
