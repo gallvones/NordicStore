6a5e7cb896596a12098352c4bd73d98bf09a6263
Luiz Galvão <luizmedeiros327@gmail.com>
2025-03-13 17:24:49 -0300

    Changin the route of index.html from biuld

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, '../build')));
 
 // Rota padrão para o front end
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../store/biuld', 'index.html'));
 });
 
 // DB connect
